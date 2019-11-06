25340e92d4f4ed0e4c345e894f7f42854860091c
mstbeat <mrbrightside82@ymail.com>
2019-11-06 14:45:58 +0900

    Implement Ajax

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
+  def created_at
+    attributes['created_at'].strftime("%Y/%m/%d(%a) %H:%M:%S")
+  end
 end
